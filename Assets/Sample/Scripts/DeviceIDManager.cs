3a02bd3c0d7ba08a21b6bfdc483dc94043e18405
y-ka <y-ka@adglobe.co.jp>
2016-03-11 17:02:10 +0900

    init

---
+++ Assets/Sample/Scripts/DeviceIDManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class DeviceIDManager : MonoBehaviour {
+
+	public Text deviceID = null;
+
+	[DllImport("__Internal")]
+	static extern string _Get_Device_id();
+
+	// Use this for initialization
+	void Start () {
+		string password = _Get_Device_id();
+		if (!string.IsNullOrEmpty(password)) {
+			deviceID.text = password;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
