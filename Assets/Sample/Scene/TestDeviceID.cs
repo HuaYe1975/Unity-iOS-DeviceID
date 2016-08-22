db1e8a1910d0245dca8e27973a262d3a11d05652
y-ka <y-ka@adglobe.co.jp>
2016-08-22 11:31:14 +0900

    add get android device id(mac address)

---
+++ Assets/Sample/Scene/TestDeviceID.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestDeviceID : MonoBehaviour {
+	public Text deviceID = null;
+	// Use this for initialization
+	void Start () {
+		string password = DeviceIDManager.GetDeviceID();
+		if (!string.IsNullOrEmpty(password)) {
+			deviceID.text = password;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
